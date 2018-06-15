c1b8f2ea7d81cad43b5b48fa76a0a5b5039bec88
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-06-15 14:53:44 +0200

    init window

--- OGL/OGL/Program.cs
+++ OGL/OGL/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tao.FreeGlut;
+using OpenGL;
 
 namespace OGL
 {
     class Program
     {
+        private static int width = 800, height = 600, posX = 200, posY = 200;
+        private static string name = "New OGL window";
         static void Main(string[] args)
         {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutInitWindowPosition(posX, posY);
+            Glut.glutCreateWindow(name);
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            Glut.glutMainLoop();
+        }
+
+        private static void OnDisplay()
+        {
+        }
+
+        private static void OnRenderFrame()
+        {
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Glut.glutSwapBuffers();
         }
     }
 }
